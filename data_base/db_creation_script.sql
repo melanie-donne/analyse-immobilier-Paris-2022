ef0590a7841719c81c7b3e054ab94b8cce1680ea
Mélanie D <melanie.donne1993@gmail.com>
2024-03-26 13:39:48 +0100

    fix script et noms tables

--- data_base/db_creation_script.sql
+++ data_base/db_creation_script.sql
@@ -1,63 +1,66 @@
 CREATE TABLE ValeursFoncieresParis2022 (
-    ID INT PRIMARY KEY,
-    Adresse VARCHAR(255),
-    CodePostal VARCHAR(10),
-    Surface FLOAT,
-    ValeurFonciere FLOAT,
-    DateMutation DATE
+	id_valeurFonciere INT PRIMARY KEY,
+	Adresse VARCHAR(255),
+	CodePostal VARCHAR(10),
+	Surface FLOAT,
+	ValeurFonciere FLOAT,
+	DateMutation DATE,
+	CodeArrondissement INT,
+	FOREIGN KEY (CodeArrondissement) REFERENCES Arrondissements(CodeArrondissement)
 );
 
 CREATE TABLE Arrondissements (
-    CodeArrondissement INT PRIMARY KEY,
-    NomArrondissement VARCHAR(255)
+	id_arrondissement INT PRIMARY KEY,
+	NomArrondissement VARCHAR(255)
 );
 
 CREATE TABLE EspacesVertsAssimiles (
-    ID INT PRIMARY KEY,
-    Nom VARCHAR(255),
-    Superficie FLOAT,
-    Perimetre FLOAT,
-    Longitude FLOAT,
-    Latitude FLOAT
+	id_espaceVertAssimile INT PRIMARY KEY,
+	Nom VARCHAR(255),
+	Superficie FLOAT,
+	Perimetre FLOAT,
+	Longitude FLOAT,
+	Latitude FLOAT
 );
 
 CREATE TABLE LogementEncadrementLoyers (
-    ID INT PRIMARY KEY,
-    CodePostal VARCHAR(10),
-    Surface FLOAT,
-    LoyerMensuel FLOAT
+	id_logementEncadrementLoyers INT PRIMARY KEY,
+	CodePostal VARCHAR(10),
+	Surface FLOAT,
+	LoyerMensuel FLOAT
 );
 
 CREATE TABLE PLUEspacesLibresVegetaliser (
-    ID INT PRIMARY KEY,
-    Superficie FLOAT,
-    Perimetre FLOAT,
-    Longitude FLOAT,
-    Latitude FLOAT
+	id_PLUEspaceLibreVegetaliser INT PRIMARY KEY,
+	Superficie FLOAT,
+	Perimetre FLOAT,
+	Longitude FLOAT,
+	Latitude FLOAT
 );
 
 CREATE TABLE PLUSecteursRisquesPPRI (
-    ID INT PRIMARY KEY,
-    Zonage VARCHAR(10),
-    N_sq_PPRIzone INT,
-    Surface FLOAT,
-    Perimetre FLOAT,
-    PointsDeGeometrie GEOMETRY,
-    Longitude FLOAT,
-    Latitude FLOAT
+	id_PLUSecteurRisquePPRI INT PRIMARY KEY,
+	Zonage VARCHAR(10),
+	N_sq_PPRIzone INT,
+	Surface FLOAT,
+	Perimetre FLOAT,
+	PointsDeGeometrie GEOMETRY,
+	Longitude FLOAT,
+	Latitude FLOAT
 );
 
 CREATE TABLE QuartiersAdministratifs (
-    ID INT PRIMARY KEY,
-    CodeQuartier VARCHAR(10),
-    CodeQuartierINSEE VARCHAR(10),
-    NomQuartier VARCHAR(255),
-    CodeArrondissement INT,
-    CodeArrondissementINSEE VARCHAR(10),
-    Perimetre FLOAT,
-    Surface FLOAT,
-    PointsDeGeometrie GEOMETRY,
-    Longitude FLOAT,
-    Latitude FLOAT,
-    FOREIGN KEY (CodeArrondissement) REFERENCES Arrondissements(CodeArrondissement)
+	id_quartierAdministratif INT PRIMARY KEY,
+	CodeQuartier VARCHAR(10),
+	CodeQuartierINSEE VARCHAR(10),
+	NomQuartier VARCHAR(255),
+	CodeArrondissement INT,
+	CodeArrondissementINSEE VARCHAR(10),
+	Perimetre FLOAT,
+	Surface FLOAT,
+	PointsDeGeometrie GEOMETRY,
+	Longitude FLOAT,
+	Latitude FLOAT,
+	FOREIGN KEY (CodeArrondissement) REFERENCES Arrondissements(CodeArrondissement)
 );
+
